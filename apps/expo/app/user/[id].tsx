fc60a578186f9cdde79e4b94c230a0f5ed2b48a5
vivek26anand <vivek26anand.va@gmail.com>
2023-09-23 02:01:51 +0530

    a

--- apps/expo/app/user/[id].tsx
+++ apps/expo/app/user/[id].tsx
@@ -6,7 +6,7 @@ export default function Screen() {
     <>
       <Stack.Screen
         options={{
-          title: 'User',
+          title: 'Signup',
         }}
       />
       <UserDetailScreen />
